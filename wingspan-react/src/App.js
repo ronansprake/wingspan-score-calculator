329b99fcc274d19cb8003ec9a459ff486c3dc86e
Ronan Sprake <ronansprake@gmail.com>
2024-11-12 21:44:40 +0000

    Add source files

--- wingspan-react/src/App.js
+++ wingspan-react/src/App.js
@@ -13,7 +13,7 @@ function App() {
        FixedHeader();
    });
 
-	const scoreRows = 9;
+  const scoreRows = 9;
   const [currentPlayerCount, setCurrentPlayerCount] = useState(2);
   const [currentScores, setCurrentScores] = useState(Array.from({length: scoreRows},()=> Array.from({length: 5}, () => 0)));
   
@@ -26,8 +26,8 @@ function App() {
   };
 
   const getTotalScoreByPlayer = (player) => {
-  	let score = 0;
-  	currentScores.map( (text) => { score += text[player] } );
+    let score = 0;
+    currentScores.map( (text) => { score += text[player] } );
     return score;
   };
 
@@ -35,39 +35,39 @@ function App() {
     setCurrentScores(Array.from({length: scoreRows},()=> Array.from({length: 5}, () => 0)));
   };
 
-	return (
-		<div className="wrap">
-			<h1>Wingspan score calculator</h1>
-			<PlayerCountDisplay currentPlayerCount={currentPlayerCount} setCurrentPlayerCount={setCurrentPlayerCount} resetScores={resetScores} />
-			<PlayerInitialsDisplay currentPlayerCount={currentPlayerCount} />
-			<div className="player-a">
-	      <h2 className="group-label">Automa (x3/4/5)</h2>
-	      <ul className="rows">
-	        <ScoringRow currentPlayerCount={currentPlayerCount} currentScores={currentScores} changeScore={changeScore} rowLabel="Face down" rowNumber="1" disabled="1" />
-	      </ul>
-	    </div>
-			<h2 className="group-label">Amount on cards</h2>
+  return (
+    <div className="wrap">
+      <h1>Wingspan score calculator</h1>
+      <PlayerCountDisplay currentPlayerCount={currentPlayerCount} setCurrentPlayerCount={setCurrentPlayerCount} resetScores={resetScores} />
+      <PlayerInitialsDisplay currentPlayerCount={currentPlayerCount} />
+      <div className="player-a">
+        <h2 className="group-label">Automa (x3/4/5)</h2>
+        <ul className="rows">
+          <ScoringRow currentPlayerCount={currentPlayerCount} currentScores={currentScores} changeScore={changeScore} rowLabel="Face down" rowNumber="1" disabled="1" />
+        </ul>
+      </div>
+      <h2 className="group-label">Amount on cards</h2>
       <ul className="rows">
         <ScoringRow currentPlayerCount={currentPlayerCount} currentScores={currentScores} changeScore={changeScore} rowLabel="Birds" rowNumber="2" disabled="0" />
         <ScoringRow currentPlayerCount={currentPlayerCount} currentScores={currentScores} changeScore={changeScore} rowLabel="Bonuses" rowNumber="3" disabled="2" />
         <ScoringRow currentPlayerCount={currentPlayerCount} currentScores={currentScores} changeScore={changeScore} rowLabel="Goals" rowNumber="4" disabled="0" />
       </ul>
-			<h2 className="group-label">1 point each</h2>
+      <h2 className="group-label">1 point each</h2>
       <ul className="rows">
         <ScoringRow currentPlayerCount={currentPlayerCount} currentScores={currentScores} changeScore={changeScore} rowLabel="Eggs" rowNumber="5" disabled="0" />
         <ScoringRow currentPlayerCount={currentPlayerCount} currentScores={currentScores} changeScore={changeScore} rowLabel="Food" rowNumber="6" disabled="2" />
         <ScoringRow currentPlayerCount={currentPlayerCount} currentScores={currentScores} changeScore={changeScore} rowLabel="Tucked" rowNumber="7" disabled="2" />
       </ul>
-			<h2 className="group-label">Each habitat 5/2</h2>
+      <h2 className="group-label">Each habitat 5/2</h2>
       <ul className="rows end">
         <ScoringRow currentPlayerCount={currentPlayerCount} currentScores={currentScores} changeScore={changeScore} rowLabel="Nectar" rowNumber="8" disabled="0" />
       </ul>
-			<div className="total-scores">
+      <div className="total-scores">
         <TotalsRow currentPlayerCount={currentPlayerCount} getTotalScoreByPlayer={getTotalScoreByPlayer} />
-			</div>
-			<p className="feedback"><a href="	//ronansprake.co.uk/board-game-score-calculators#contact" target="_blank">Could this be better? Send me feedback</a></p>
-		</div>
-	);
+      </div>
+      <p className="feedback"><a href="//ronansprake.co.uk/board-game-score-calculators#contact" target="_blank">Could this be better? Send me feedback</a></p>
+    </div>
+  );
 }
 
 export default App;
